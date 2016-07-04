f928c966caaaa8ee7209d06c3f2807f0899698f0
Mohamed Camara GUEYE <ahmed.abdbassir@gmail.com>
2016-07-04 00:56:15 +0000

    Adding month datq

--- js/app.js
+++ js/app.js
@@ -1,4 +1,67 @@
 var annee = 2016;
+var nMonth = 12;
+months = [
+	{
+		position: 1,
+		label: "Janvier",
+		nDays: 0
+	},
+	{
+		position: 2,
+		label: "Février",
+		nDays: 0
+	},
+	{
+		position: 3,
+		label: "Mars",
+		nDays: 0
+	},
+	{
+		position: 4,
+		label: "Avril",
+		nDays: 0
+	},
+	{
+		position: 5,
+		label: "Mai",
+		nDays: 0
+	},
+	{
+		position: 6,
+		label: "Juin",
+		nDays: 0
+	},
+	{
+		position: 7,
+		label: "Juillet",
+		nDays: 0
+	},
+	{
+		position: 8,
+		label: "Aout",
+		nDays: 0
+	},
+	{
+		position: 9,
+		label: "Septembre",
+		nDays: 0
+	},
+	{
+		position: 10,
+		label: "Octobre",
+		nDays: 0
+	},
+	{
+		position: 11,
+		label: "Novembre",
+		nDays: 0
+	},
+	{
+		position: 12,
+		label: "Decembre",
+		nDays: 0
+	}
+];
 var month = "1 Janv";
 var end_annee = annee+1;
 var start_string = month+" "+annee
@@ -6,8 +69,11 @@ var startDate = new Date(start_string);
 var endDate = new Date(start_string);
 endDate.setFullYear(end_annee);
 
+for (var i = Things.length - 1; i >= 0; i--) {
+	Things[i]
+}
 console.log(startDate);
 console.log(endDate);
 
-var diff = endDate.getTime() - startDate.getTime();
-console.log(diff);
+var diff = Math.abs(endDate.getTime() - startDate.getTime());
+var nDays = Math.ceil(diff / (3600*1000*24));
