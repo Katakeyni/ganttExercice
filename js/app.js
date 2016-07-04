0b5a0ecff937941d9b51f44bb0fcdc4f566e5522
Mohamed Camara GUEYE <ahmed.abdbassir@gmail.com>
2016-07-04 02:14:21 +0000

    Adding calendar and defaults data project

--- js/app.js
+++ js/app.js
@@ -62,6 +62,7 @@ months = [
 		nDays: 0
 	}
 ];
+$
 var month = "1 Janv";
 var end_annee = annee+1;
 var start_string = month+" "+annee
@@ -69,11 +70,35 @@ var startDate = new Date(start_string);
 var endDate = new Date(start_string);
 endDate.setFullYear(end_annee);
 
-for (var i = Things.length - 1; i >= 0; i--) {
-	Things[i]
-}
-console.log(startDate);
-console.log(endDate);
+for (var i = 0; i < months.length; i++) {
+	var position = months[i].position;
+	var label = months[i].label;
+	var monthView = "<li class='month'>";
+	monthView += "<div class='month_label'>"+label+"</div>";
+	monthView += "<ul class='day-month-container'>";
+	endMonth = new Date(start_string);
+	start = new Date(start_string);
+	if(i > 0) {
+		start.setMonth(position - 1 );
+	}
+	
+	endMonth.setMonth(position);
 
-var diff = Math.abs(endDate.getTime() - startDate.getTime());
-var nDays = Math.ceil(diff / (3600*1000*24));
+	nDays = getDays(start, endMonth);
+	for (var j = 1; j <= nDays; j++) {
+		monthView += "<li>"+j+"</li>";
+	}
+	monthView += "</ul>";
+
+	$('#months').append(monthView)
+
+}
+
+
+
+function getDays(start, end){
+	var diff = Math.abs(end.getTime() - start.getTime());
+	var nDays = Math.ceil(diff / (3600*1000*24));
+	return nDays;
+}
+
---
+++ js/projets.js
@@ -0,0 +1,111 @@
+var projets = [
+	{
+		nom: "Nom du projets",
+		debut: new Date("1 Jan 2016"),
+		fin: new Date("3 Mar 2016")
+		taches: [
+			{
+				nom: "Nom du projets",
+				debut: new Date("1 Jan 2016"),
+				fin: new Date("5 Jan 2016"),
+				progression: 100
+			},
+			{
+				nom: "Nom du projets",
+				debut: new Date("3 Jan 2016"),
+				fin: new Date("20 Mar 2016"),
+				progression: 100
+			},
+			{
+				nom: "Nom du projets",
+				debut: new Date("20 Jan 2016"),
+				fin: new Date("1 Feb 2016"),
+				progression: 100
+			},
+			{
+				nom: "Nom du projets",
+				debut: new Date("29 Janv 2016"),
+				fin: new Date("14 Feb 2016"),
+				progression: 95
+			},
+			{
+				nom: "Nom du projets",
+				debut: new Date("10 Feb 2016"),
+				fin: new Date("24 Feb 2016"),
+				progression: 40
+			},
+			{
+				nom: "Nom du projets",
+				debut: new Date("24 Feb 2016"),
+				fin: new Date("3 Mar 2016"),
+				progression: 40
+			},
+			{
+				nom: "Nom du projets",
+				debut: new Date("7 Mar 2016"),
+				fin: new Date("22 Mar 2016"),
+				progression: 40
+			},
+			{
+				nom: "Nom du projets",
+				debut: new Date("23 Mar 2016"),
+				fin: new Date("12 Mai 2016"),
+				progression: 40
+			},
+			{
+				nom: "Nom du projets",
+				debut: new Date("8 Mai 2016"),
+				fin: new Date("31 Mai 2016"),
+				progression: 40
+			},
+			{
+				nom: "Nom du projets",
+				debut: new Date("28 Mai 2016"),
+				fin: new Date("3 Mar 2016"),
+				progression: 40
+			},
+			{
+				nom: "Nom du projets",
+				debut: new Date("1 Jan 2016"),
+				fin: new Date("3 Mar 2016"),
+				progression: 40
+			},
+			{
+				nom: "Nom du projets",
+				debut: new Date("1 Jan 2016"),
+				fin: new Date("3 Mar 2016"),
+				progression: 40
+			},
+			{
+				nom: "Nom du projets",
+				debut: new Date("1 Jan 2016"),
+				fin: new Date("3 Mar 2016"),
+				progression: 40
+			},
+			{
+				nom: "Nom du projets",
+				debut: new Date("1 Jan 2016"),
+				fin: new Date("3 Mar 2016"),
+				progression: 40
+			},
+			{
+				nom: "Nom du projets",
+				debut: new Date("1 Jan 2016"),
+				fin: new Date("3 Mar 2016"),
+				progression: 40
+			},
+			{
+				nom: "Nom du projets",
+				debut: new Date("1 Jan 2016"),
+				fin: new Date("3 Mar 2016"),
+				progression: 40
+			},
+			{
+				nom: "Nom du projets",
+				debut: new Date("1 Jan 2016"),
+				fin: new Date("3 Mar 2016"),
+				progression: 40
+			},
+		]
+	}
+]
